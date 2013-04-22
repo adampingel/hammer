2992dbf445b68298328e2667f7a19184b9ba1382
Adam Pingel <pingel@gmail.com>
2013-04-22 00:08:31 -0700

    adding cnn

--- src/main/scala/org/pingel/hammer/ExampleLoadGenerator.scala
+++ src/main/scala/org/pingel/hammer/ExampleLoadGenerator.scala
@@ -6,13 +6,15 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class ExampleLoadGenerator extends LoadGenerator {
 
   import dispatch._ // , Defaults._
-  
-  val cnn = url("http://api.hostip.info/get_json.php")
-  
-  val country = url("http://api.hostip.info/country.php")
 
-  def randomRequestBuilder() = if (util.Random.nextBoolean) cnn else country
-  
+  val requestBuilders = Vector(
+    url("http://api.hostip.info/get_json.php"),
+    url("http://api.hostip.info/country.php"),
+    url("http://www.cnn.com/")
+  )
+
+  def randomRequestBuilder() = requestBuilders(util.Random.nextInt(requestBuilders.size))
+
   def makeNextRequest() = Http(randomRequestBuilder() OK as.String)
 
 }
